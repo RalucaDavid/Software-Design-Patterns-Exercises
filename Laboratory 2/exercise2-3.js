5a90ef00940056f7034c8b682b2cf1e2f0d980db
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2024-10-24 16:42:05 +0300

    finish labotory 2

---
+++ Laboratory 2/exercise2-3.js
@@ -0,0 +1,31 @@
+
+const deprecatedTags = [
+    'acronym', 'applet', 'basefont', 'big', 'center', 'dir', 'font',
+    'frame', 'frameset', 'noframes', 'isindex', 'listing', 'menu',
+    'marquee', 's', 'strike', 'tt', 'u', 'xmp'
+];
+
+function isDeprecated(tagName) {
+    return deprecatedTags.includes(tagName.toLowerCase());
+}
+
+function traversePreOrder(node) {
+    if (!node)
+        return;
+
+    console.log(node);
+
+    if (node.nodeType === Node.ELEMENT_NODE) {
+        const tagName = node.tagName.toLowerCase();
+        if (isDeprecated(tagName)) {
+            console.log(`Tag deprecated found: <${tagName}>`);
+        }
+    }
+    
+    node.childNodes.forEach(child => {
+        traversePreOrder(child);
+    });
+}
+
+const body = document.body;
+traversePreOrder(body); 
